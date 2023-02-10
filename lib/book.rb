65e86d0e6ea11dfcf710e80431572cecc3462705
Emilly <emilly.wanjiku@student.moringaschool.com>
2023-02-10 16:38:25 +0300

    Complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    attr_reader :turn_page
+    def initialize(title)
+        @title=title
+        @author=author
+        @page_count=page_count
+        @genre=genre
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+       def initialize(brand, condition="old")
+        @brand=brand
+        @color=color
+        @size=size
+        @material=material
+        @condition=condition
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition="new"
+    end
+
+end
